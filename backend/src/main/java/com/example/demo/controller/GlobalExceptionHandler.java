c1341c0b561192f135d1a957d463c20b341e14a7
Aristarh Ucolov <aristarh.ucolov@gmail.com>
2025-11-08 11:36:57 +0200

    Fixed

--- backend/src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
@@ -1,14 +1,14 @@
-package com.example.demo.exception;
+package com.example.demo.controller;
 
-import com.example.demo.exception;
+import com.example.demo.exception.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.security.access.AccessDeniedException;
 
-import java.tio.file.AcesssDeniedException
-
-@ResetControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccessDeniedException.class)
